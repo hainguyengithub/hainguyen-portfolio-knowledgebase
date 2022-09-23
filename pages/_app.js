e1348866ec567701980bf2666e7332da91225cde
Hai Nguyen <hnguyen@northern.co>
2022-09-23 06:09:48 -0400

    adapt the basic pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '@styles/globals.css'
+import Layout from "@components/Layout";
+import "@styles/globals.css";
+import { Component } from "react";
+
+// function Application({ Component, pageProps }) {
+//     return <Component {...pageProps} />;
+// }
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default Application
+export default Application;
--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,11 @@
-import Header from "@components/Header";
+import Header from '@components/Header';
+import React from 'react';
 
-export default function Home() {
+export default function About(props) {
   return (
     <div className="container">
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Hai Nguyen" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
