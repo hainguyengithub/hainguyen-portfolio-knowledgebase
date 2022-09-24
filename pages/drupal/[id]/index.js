94a8b785c966936816ea898aaf5e18d325ab4f3b
Hai Nguyen <hnguyen@northern.co>
2022-09-24 18:22:34 -0400

    fake /drupal api to get Drupal articles

---
+++ pages/drupal/[id]/index.js
@@ -0,0 +1,30 @@
+import Header from "@components/Header";
+import { server } from "config";
+import React from "react";
+
+function DrupalIndex(props) {
+  return (
+    <div>
+      <Header title={props.articles.title} />
+    </div>
+  );
+}
+
+export const getStaticProps = async (context) => {
+  const result = await fetch(`${server}/api/drupal/${context.params.id}`);
+  const articles = await result.json();
+  return {
+    props: {
+      articles,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
+    fallback: false
+  };
+};
+
+export default DrupalIndex;
