86493194fb515d6310cf884ce77d093f7c52c57b
Hai Nguyen <hnguyen@northern.co>
2022-09-24 21:26:51 -0400

    update the production URL

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : "https://yourwebsite.com";
+export const server = dev ? "http://localhost:3000" : "https://zesty-sundae-b9fab7.netlify.app";
