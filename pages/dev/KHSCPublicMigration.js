9343ffd62353d514fff7800712ad802274bc9169
Hai Nguyen <hai.nguyen.phc@gmail.com>
2023-03-17 09:09:20 -0400

    enhance the KHSC public migration component

--- pages/dev/KHSCPublicMigration.js
+++ pages/dev/KHSCPublicMigration.js
@@ -1,14 +1,16 @@
-// @see pages/_app.js
+// @see pages/_app.js where Bootstrap is imported as global styles.
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import { useState } from 'react';
-import helloWorldStyles from './KHSCPublicMigration.module.css';
+import styles from './KHSCPublicMigration.module.css';
 
 // http://localhost:3000/dev/KHSCPublicMigration
 export default function () {
   const [command, setCommand] = useState('');
   const [migration, setMigration] = useState('');
   const [showAlert, setShowAlert] = useState(false);
+  const [importLimit, setImportLimit] = useState(1);
   const commands = [
     'rollback',
     'reset',
@@ -19,16 +21,22 @@ export default function () {
     'node_profile',
     'taxonomy_term_researcher_type',
   ];
+  const buildFullCommand = () => {
+    return command === 'import'
+      ? `ddev drush migrate:${command} ${migration} --limit=${importLimit}`
+      : `ddev drush migrate:${command} ${migration}`
+  }
   return (
-    <div id={helloWorldStyles.root}>
+    <div id={styles.root}>
       <h1>Command Line Interface</h1>
       <Alert variant="primary" show={showAlert} onClose={() => { setShowAlert(false) }} dismissible>Command copied to clipboard.</Alert>
-      <div id={helloWorldStyles.appMigration} className='app-container'>
-        <div className={helloWorldStyles.container}>
+      <div id={styles.appMigration} className='app-container'>
+        {/* Command column */}
+        <div className={styles.container}>
           <div>Commands</div>
           <div>
-            <ul className={helloWorldStyles.list} >
-              {commands.map(each => <li onClick={() => { setCommand(each) }}>
+            <ul className={styles.list} >
+              {commands.map(each => <li key={`li-${each}`} onClick={() => { setCommand(each); }}>
                 <input
                   id={each}
                   value={each}
@@ -38,38 +46,41 @@ export default function () {
                 // Warning: You provided a `checked` prop to a form field without an `onChange` handler.
                 // checked={each === command}
                 />
-                <label className={helloWorldStyles.label} htmlFor={each}>
+                <label className={styles.label} htmlFor={each}>
                   {each}
                 </label>
               </li>)}
             </ul>
           </div>
         </div>
-        <div className={helloWorldStyles.container}>
+        {/* Option column */}
+        {command === 'import' && <div className={`${styles.container} ${styles.thin}`}><Form.Control placeholder="Limit" value={importLimit} onChange={(evt) => { console.log(evt.target.value.length); setImportLimit(evt.target.value.length === 0 ? 1 : evt.target.value) }} aria-label="Limit for import command" aria-describedby="limit-for-import-command" /></div>}
+        {/* Migration column */}
+        <div className={styles.container}>
           <div>Migrations</div>
           <div>
-            <ul className={helloWorldStyles.list}>
-              {migrations.map(each => <li onClick={() => { setMigration(each) }}>
+            <ul className={styles.list}>
+              {migrations.map(each => <li key={`li-${each}`} onClick={() => { setMigration(each) }}>
                 <input
                   id={each}
                   value={each}
                   type="radio"
                   name='migrate-migration'
                 />
-                <label className={helloWorldStyles.label} htmlFor={each}>
+                <label className={styles.label} htmlFor={each}>
                   {each}
                 </label>
               </li>)}
             </ul>
           </div>
         </div>
-        <div className={helloWorldStyles.container}>
-          <Button variant='primary' onClick={() => { navigator.clipboard.writeText(`ddev drush migrate:${command} ${migration}`); setShowAlert(true); }}>Done</Button>
+        {/* Done button */}
+        <div className={styles.container}>
+          <Button variant='primary' disabled={command === '' || migration === ''} onClick={() => { navigator.clipboard.writeText(buildFullCommand()); setShowAlert(true); }}>Done</Button>
         </div>
       </div>
-      <code id="result">
-        ddev drush migrate:<span className={helloWorldStyles.emphasis}>{command}</span> <span className={helloWorldStyles.emphasis}>{migration}</span>
-      </code>
+      {/* The fully-built command */}
+      <code id={styles.result}>{buildFullCommand()}</code>
     </div>
-  );
+  ); // end of return
 }
